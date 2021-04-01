032dead6f0fa2efba1e4270cee2cb086ccde75d1
Luci Temple <lucitemple@yahoo.com.au>
2021-04-01 23:01:01 +1100

    coreui files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import './scss/style.scss';
 
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+)
 
-import Navbar from './components/Navbar/Navbar.js';
-import Landing from './components/Landing/Landing.js';
-import Expenses from './components/Expenses/Expenses.js';
-import ReportAudit from './components/ReportAudit/ReportAudit.js';
-import ClientList from './components/ClientManagement/ClientList.js';
-import ClientManagement from './components/ClientManagement/ClientManagement.js';
+// Containers
+const TheLayout = React.lazy(() => import('./containers/TheLayout'));
 
-class App extends React.Component {
-  render(){
+// Pages
+const Login = React.lazy(() => import('./views/pages/login/Login'));
+const Register = React.lazy(() => import('./views/pages/register/Register'));
+const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
+const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
+
+class App extends Component {
+
+  render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/report' component={ReportAudit} />
-          <Route exact path='/expenses' component={Expenses} />
-          <Route exact path='/clist' component={ClientList} />
-          <Route exact path='/cmanagement' component={ClientManagement} />
-        </div>
-      </BrowserRouter>  
+      <HashRouter>
+          <React.Suspense fallback={loading}>
+            <Switch>
+              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
+              <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
+              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
+              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
+              <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
+            </Switch>
+          </React.Suspense>
+      </HashRouter>
     );
   }
 }
