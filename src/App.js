639d9edd2bba6c41ccf1a5a05d1a0531753e8f2a
Nidvar <djayshine87@hotmail.com>
2021-04-09 21:10:24 +1000

    home page is now the login page. Login page had all unneccessary items removed

--- src/App.js
+++ src/App.js
@@ -24,11 +24,11 @@ class App extends Component {
       <HashRouter>
           <React.Suspense fallback={loading}>
             <Switch>
-              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
+              <Route exact path="/" name="Login Page" render={props => <Login {...props}/>} />
               <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
               <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
               <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
+              <Route exact path="/dashboard" name="Home" render={props => <TheLayout {...props}/>} />
             </Switch>
           </React.Suspense>
       </HashRouter>
